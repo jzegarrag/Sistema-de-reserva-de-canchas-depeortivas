f1b43a26e9c5aa1d5da148a62513fbe6b58ec954
jzegarrag <jzegarrag@unjbg.edu.pe>
2026-02-23 22:33:16 -0500

    Actualización general del sistema de reservas

---
+++ reserva.ts
@@ -0,0 +1,24 @@
+
+import { Usuario } from "./Usuario";
+import { Cancha } from "./Canchas";
+
+export class Reserva {
+  constructor(
+    private usuario: Usuario,
+    private cancha: Cancha,
+    private fecha: string,
+    private hora: string
+  ) {}
+
+  getDetalle(): string {
+    return `${this.usuario.getNombre()} - ${this.cancha.getNombre()} - ${this.fecha} ${this.hora}`;
+  }
+
+  getFecha(): string {
+    return this.fecha;
+  }
+
+  getHora(): string {
+    return this.hora;
+  }
+}
